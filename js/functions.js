09dc2b88e8ca4c337ac2bbc84513779503f116d0
Арина Тюрина <arisha666666@gmail.com>
2025-10-20 13:44:16 +0500

    5.9

--- js/functions.js
+++ js/functions.js
@@ -28,3 +28,16 @@ function extractNumber(str) {
   const digits = str.replace(/\D/g, '');
   return digits ? parseInt(digits, 10) : NaN;
 }
+
+function timeToMinutes(timeString) {
+  const [hours, minutes] = timeString.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+function isMeetingWithinWorkHours(workStart, workEnd, meetingStart, meetingDuration) {
+  const workStartMinutes = timeToMinutes(workStart);
+  const workEndMinutes = timeToMinutes(workEnd);
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+  return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
+}
