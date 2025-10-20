cf47a6890985d87d127b264528f9d985d43cf90d
Арина Тюрина <arisha666666@gmail.com>
2025-10-20 13:48:25 +0500

    5.9

---
+++ js/data/utils.js
@@ -0,0 +1,20 @@
+export function getRandomElement (array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+export function getRandomInteger (min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const generateUniqueId = () => {
+  const usedIds = new Set();
+
+  return () => {
+    let id;
+    do {
+      id = getRandomInteger(1, 1000);
+    } while (usedIds.has(id));
+    usedIds.add(id);
+    return id;
+  };
+};
