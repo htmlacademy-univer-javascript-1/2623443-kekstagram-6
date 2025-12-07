b2aae737829635387964b045513ce43395a8b6e6
Арина Тюрина <arisha666666@gmail.com>
2025-12-07 23:31:01 +0500

    7.11

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import { generatePhotosArray } from './data/data-generation.js';
+import { renderThumbnails } from './thumbnails.js';
 
 const photos = generatePhotosArray();
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderThumbnails();
+});
 
 export { photos };
---
+++ js/thumbnails.js
@@ -0,0 +1,36 @@
+import { photos } from './main.js';
+
+const renderThumbnails = () => {
+  const picturesContainer = document.querySelector('.pictures');
+  const pictureTemplate = document.querySelector('#picture');
+
+  if (!picturesContainer || !pictureTemplate) {
+    return;
+  }
+
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const thumbnailElement = pictureTemplate.content.cloneNode(true);
+    const thumbnail = thumbnailElement.querySelector('.picture');
+    const image = thumbnail.querySelector('.picture__img');
+    const commentsElement = thumbnail.querySelector('.picture__comments');
+    const likesElement = thumbnail.querySelector('.picture__likes');
+
+    image.src = photo.url;
+    image.alt = photo.description;
+    commentsElement.textContent = photo.comments.length;
+    likesElement.textContent = photo.likes;
+
+    thumbnail.dataset.photoId = photo.id;
+
+    fragment.appendChild(thumbnailElement);
+  });
+
+  const existingPictures = picturesContainer.querySelectorAll('.picture');
+  existingPictures.forEach(picture => picture.remove());
+
+  picturesContainer.appendChild(fragment);
+};
+
+export { renderThumbnails };
